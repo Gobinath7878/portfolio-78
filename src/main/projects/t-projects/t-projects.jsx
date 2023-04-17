4a9e74ebb9d3bd8d68c86b6bbaf26783f9cfa3ce
Gobinath S <115769414+Gobinath7878@users.noreply.github.com>
2023-04-17 18:41:04 +0530

    Update t-projects.jsx

--- src/main/projects/t-projects/t-projects.jsx
+++ src/main/projects/t-projects/t-projects.jsx
@@ -4,9 +4,12 @@ import BubbleChartIcon from '@mui/icons-material/BubbleChart';
 
 const ProjectsTitle = () => {
   return (
+       <>
     <div className='projects-title'> 
-     <h1 className="home-title"><span>pro</span>j<span>ects</span><BubbleChartIcon sx={{color:"black",fontSize:"45px"}}/></h1> 
+     <h1 className="home-title"><span>pro</span>j<span>ects</span><BubbleChartIcon sx={{color:"black",fontSize:"45px"}}/></h1>
     </div>
+    <h6 className='credentials-text'>Admin Email- chegoa@gmail.com & password- chegoa</h6>
+    </>
   )
 }
 
