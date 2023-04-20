1c25359b3c8f8b4b8b357cebf3909262cfec17b5
Gobinath S <115769414+Gobinath7878@users.noreply.github.com>
2023-04-20 13:59:19 +0530

    Update c-projects.jsx

--- src/main/projects/c-projects/c-projects.jsx
+++ src/main/projects/c-projects/c-projects.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./c-projects.css";
-import Nav from "react-bootstrap/Nav";
 import Drinksbuddy from '../../../assests/about/chegostore-project-Img.png';
 import Chatapp from '../../../assests/about/chatapp-project-Img.png'
 import Tourapp from '../../../assests/about/tourapp-project-Img.png'
@@ -20,9 +19,9 @@ const ProjectsContent = () => {
               alt="loading"
             />
             <h4>Drinks Buddy</h4>
-            <Nav.Link href="https://chego-store.netlify.app" className="pro-btn">Live site</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/cheGo-store-fe" className="pro-btn">Frond-End</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/cheGo-store-be" className="pro-btn">Back-End</Nav.Link>
+            <a href="https://chego-store.netlify.app" target="_blank" rel="noreferrer" className="pro-btn">Live site</a>
+            <a href="https://github.com/Gobinath7878/cheGo-store-fe" target="_blank" rel="noreferrer" className="pro-btn">Frond-End</a>
+            <a href="https://github.com/Gobinath7878/cheGo-store-be" target="_blank" rel="noreferrer" className="pro-btn">Back-End</a>
           </div>
         </div>
         <div className="box">
@@ -32,9 +31,9 @@ const ProjectsContent = () => {
               alt="loading"
             />
             <h4>Chat App</h4>
-            <Nav.Link href="https://chat-fe-8kpa.onrender.com" className="pro-btn">Live site</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/chat-fe" className="pro-btn">Frond-End</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/chat-be" className="pro-btn">Back-End</Nav.Link>
+            <a href="https://chat-fe-8kpa.onrender.com" target="_blank" rel="noreferrer" className="pro-btn">Live site</a>
+            <a href="https://github.com/Gobinath7878/chat-fe" target="_blank" rel="noreferrer" className="pro-btn">Frond-End</a>
+            <a href="https://github.com/Gobinath7878/chat-be" target="_blank" rel="noreferrer" className="pro-btn">Back-End</a>
           </div>
         </div>
         <div className="box">
@@ -44,9 +43,9 @@ const ProjectsContent = () => {
               alt="loading"
             />
             <h4>Tour App</h4>
-            <Nav.Link href="https://travego-na.netlify.app" className="pro-btn">Live site</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/Tour-app-FE" className="pro-btn">Frond-End</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/Tour-App-BE" className="pro-btn">Back-End</Nav.Link>
+            <a href="https://travego-na.netlify.app" target="_blank" rel="noreferrer" className="pro-btn">Live site</a>
+            <a href="https://github.com/Gobinath7878/Tour-app-FE" target="_blank" rel="noreferrer" className="pro-btn">Frond-End</a>
+            <a href="https://github.com/Gobinath7878/Tour-App-BE" target="_blank" rel="noreferrer" className="pro-btn">Back-End</a>
           </div>
         </div>
 
@@ -57,9 +56,9 @@ const ProjectsContent = () => {
               alt="loading"
             />
             <h4>EliteMail Pro</h4>
-            <Nav.Link href="https://bulk-email-tool.netlify.app" className="pro-btn">Live site</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/Bulk-Email-Tool-Frontend" className="pro-btn">Frond-End</Nav.Link>
-            <Nav.Link href="https://github.com/Gobinath7878/Bulk-Email-Tool-Backend" className="pro-btn">Back-End</Nav.Link>
+            <a href="https://bulk-email-tool.netlify.app" target="_blank" rel="noreferrer" className="pro-btn">Live site</a>
+            <a href="https://github.com/Gobinath7878/Bulk-Email-Tool-Frontend" target="_blank" rel="noreferrer" className="pro-btn">Frond-End</a>
+            a href="https://github.com/Gobinath7878/Bulk-Email-Tool-Backend" target="_blank" rel="noreferrer" className="pro-btn">Back-End</a>
           </div>
         </div>
       </div>
